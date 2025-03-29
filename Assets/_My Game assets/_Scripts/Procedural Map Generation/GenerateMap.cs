df14cde5e61294cded8d740bb7fd851a91b2bd4e
VishalMaurya0 <vishalmaurya22.24@kgpian.iitkgp.ac.in>
2025-03-30 01:48:21 +0530

    Centre has been done in map Generation

--- Assets/_My Game assets/_Scripts/Procedural Map Generation/GenerateMap.cs
+++ Assets/_My Game assets/_Scripts/Procedural Map Generation/GenerateMap.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerateMap : MonoBehaviour
@@ -11,10 +9,14 @@ public class GenerateMap : MonoBehaviour
     [Header("Inputs")]
     public GameObject CellObj;
     public List<GameObject> walls;
+    public int roomMinLength;
+    public int roomMaxLength;
+    public int totalRooms;
 
     [Header("Map Properties")]
-    private MapCell[,] mapGrid;
-    public MapCell centreMapCell;
+    public MapCell[,] mapCells;
+    private MapCell centreMapCell;
+    private List<MapCell> InitialGateRooms;
 
     public bool updateVisual;
 
@@ -23,97 +25,269 @@ public class GenerateMap : MonoBehaviour
         grid = GetComponent<Grid>();
         CreateInitialMap();
         ReferenceAdjacentCells();
-    }
-
-    private void Update()
-    {
-        if (updateVisual)
-        {
-            //UpdateVisual();
-        }
-    }
-
-    private void ReferenceAdjacentCells()
-    {
-        int rowCount = mapGrid.GetLength(0);
-        int columnCount = mapGrid.GetLength(1);
-
-        for (int i = 0; i < rowCount; i++)
-        {
-            for (int j = 0; j < columnCount; j++)
-            {
-                MapCell cell = mapGrid[i, j];
-
-                if (j + 1 < columnCount) cell.adjCell[0] = mapGrid[i, j + 1]; // Right
-                if (i - 1 >= 0) cell.adjCell[1] = mapGrid[i - 1, j]; // Top
-                if (j - 1 >= 0) cell.adjCell[2] = mapGrid[i, j - 1]; // Left
-                if (i + 1 < rowCount) cell.adjCell[3] = mapGrid[i + 1, j]; // Bottom
-            }
-        }
+        StartGeneration();
     }
 
     private void CreateInitialMap()
     {
         int rows = grid.length / grid.cellLength;
         int cols = grid.width / grid.cellLength;
-        mapGrid = new MapCell[rows, cols]; // Allocate fixed-size array
+        mapCells = new MapCell[rows, cols]; // Allocate fixed-size array
 
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++)
             {
                 Cell cell = grid.cells[j + (i * cols)];
-                mapGrid[i, j] = new MapCell(cell);
+                mapCells[i, j] = new MapCell(cell);
             }
         }
     }
 
-    private void ChooseGate()
+    private void ReferenceAdjacentCells()
     {
-        int centerRow = grid.length / grid.cellLength / 2;
-        int centerCol = grid.width / grid.cellLength / 2;
+        int rowCount = mapCells.GetLength(0);
+        int columnCount = mapCells.GetLength(1);
 
-        System.Random rand = new System.Random();
-        int[] gateOffsets = { -1, 0, 1 };
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                MapCell cell = mapCells[i, j];
 
-        // North Gate
-        int northGateCol = centerCol + gateOffsets[rand.Next(0, 3)];
-        mapGrid[centerRow - 2, northGateCol].wall[3] = WallType.gate;
-        mapGrid[centerRow - 1, northGateCol].wall[1] = WallType.gate;
-
-        // South Gate
-        int southGateCol = centerCol + gateOffsets[rand.Next(0, 3)];
-        mapGrid[centerRow + 2, southGateCol].wall[1] = WallType.gate;
-        mapGrid[centerRow + 1, southGateCol].wall[3] = WallType.gate;
-
-        // East Gate
-        int eastGateRow = centerRow + gateOffsets[rand.Next(0, 3)];
-        mapGrid[eastGateRow, centerCol + 2].wall[2] = WallType.gate;
-        mapGrid[eastGateRow, centerCol + 1].wall[0] = WallType.gate;
-
-        // West Gate
-        int westGateRow = centerRow + gateOffsets[rand.Next(0, 3)];
-        mapGrid[westGateRow, centerCol - 2].wall[0] = WallType.gate;
-        mapGrid[westGateRow, centerCol - 1].wall[2] = WallType.gate;
+                if (i + 1 < rowCount) cell.adjCell[0] = mapCells[i + 1, j]; // Right
+                if (j + 1 < columnCount) cell.adjCell[1] = mapCells[i, j + 1]; // Top
+                if (i - 1 >= 0) cell.adjCell[2] = mapCells[i - 1, j]; // Left
+                if (j - 1 >= 0) cell.adjCell[3] = mapCells[i, j - 1]; // Bottom
+            }
+        }
     }
+
+
+    private void StartGeneration()
+    {
+        CentreGeneration();
+        ChooseGate();
+        GenerateRooms(roomMinLength, roomMaxLength, totalRooms);
+        GeneratePath();
+        Debug.Log(mapCells[0, 1].wall[2]);
+    }
+
+    private void Update()
+    {
+        if (updateVisual)
+        {
+            UpdateVisual();
+        }
+    }
+    private void UpdateVisual()
+    {
+        updateVisual = false;
+
+        int rowCount = mapCells.GetLength(0);
+        int columnCount = mapCells.GetLength(1);
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                GameObject obj = Instantiate(CellObj, transform);
+                obj.transform.position = mapCells[i, j].position;
+                obj.transform.localScale = Vector3.one * (grid.cellLength - 0.2f);
+                obj.name = $"Cell ({j}, {i})";
+                mapCells[i, j].cellObject = obj;
+            }
+        }
+
+        //Wall Visual
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                MapCell cell = mapCells[i, j];
+
+                for (int k = 0; k < cell.wall.Length; k++)
+                {
+                    if (cell.wall[k] == WallType.noWall) Destroy(cell.wallG[k]);
+
+                    if (cell.wallG[k] == null && cell.wall[k] != WallType.noWall)
+                    {
+                        // Get the wall prefab from the list based on wall type
+                        GameObject wallPrefab = walls[(int)cell.wall[k]];
+
+                        if (wallPrefab != null)
+                        {
+                            // Instantiate the wall at the appropriate position & rotation
+                            GameObject newWall = Instantiate(wallPrefab, cell.GetWallPosition(k, cell), cell.GetWallRotation(k));
+
+                            // Store the reference
+                            cell.wallG[k] = newWall;
+                            int oppositeIndex = (k + 2) % 4; // 0↔2 (right-left), 1↔3 (top-bottom)
+
+                            if (cell.adjCell[k] != null)
+                            {
+                                cell.adjCell[k].wallG[oppositeIndex] = newWall;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+    void RemoveWalls(MapCell cell, int right, int top, int left, int bottom)
+    {
+        int[] directions = { right, top, left, bottom };
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (directions[i] > -1)
+            {
+                switch (directions[i])
+                {
+                    case 0: cell.wall[i] = WallType.noWall; break;
+                    case 1: cell.wall[i] = WallType.wall; break;
+                    case 2: cell.wall[i] = WallType.gate; break;
+                    case 3: cell.wall[i] = WallType.window; break;
+                }
+
+                // Update adjacent cell's corresponding wall
+                if (cell.adjCell[i] != null)
+                {
+                    int oppositeIndex = (i + 2) % 4; // 0↔2 (right-left), 1↔3 (top-bottom)
+                    cell.adjCell[i].wall[oppositeIndex] = cell.wall[i];
+                }
+            }
+        }
+    }
+
+
 
     private void CentreGeneration()
     {
         int centerRow = grid.length / grid.cellLength / 2;
         int centerCol = grid.width / grid.cellLength / 2;
 
+        centreMapCell = mapCells[centerRow, centerCol];
+
         for (int i = -1; i <= 1; i++)
         {
             for (int j = -1; j <= 1; j++)
             {
-                MapCell cell = mapGrid[centerRow + i, centerCol + j];
+                MapCell cell = mapCells[centerRow + i, centerCol + j];
 
-                if (j < 1) cell.wall[0] = WallType.noWall; // Right
-                if (j > -1) cell.wall[2] = WallType.noWall; // Left
-                if (i > -1) cell.wall[1] = WallType.noWall; // Top
-                if (i < 1) cell.wall[3] = WallType.noWall; // Bottom
+                if (i < 1) cell.wall[0] = WallType.noWall; // Right
+                if (i > -1) cell.wall[2] = WallType.noWall; // Left
+                if (j > -1) cell.wall[3] = WallType.noWall; // Top
+                if (j < 1) cell.wall[1] = WallType.noWall; // Bottom
+
+                cell.inRoom = true;
             }
         }
+    }
+    private void ChooseGate()
+    {
+        int rowCount = mapCells.GetLength(0);
+        int colCount = mapCells.GetLength(1);
+
+        int centerRow = rowCount / 2;
+        int centerCol = colCount / 2;
+
+        System.Random rand = new System.Random();
+
+        for (int i = -1; i < 2; i += 2)   ///choosing direction Left and Right
+        {
+            int j = rand.Next(-1, 2);   // -1 , 0, 1 
+            MapCell cell = mapCells[i + centerRow, j + centerCol];
+            switch(i)
+            {
+                case -1: RemoveWalls(cell, -1, -1, 2, -1); break;
+                case 1: RemoveWalls(cell, 2, -1, -1, -1); break;
+            }
+            InitialGateRooms.Add(cell);
+            cell.inRoom = true;
+        }
+        
+        for (int i = -1; i < 2; i += 2)   ///choosing direction North and south
+        {
+            int j = rand.Next(-1, 2);   // -1 , 0, 1 
+            MapCell cell = mapCells[j + centerRow, i + centerCol];
+            switch(i)
+            {
+                case -1: RemoveWalls(cell, -1, -1, -1, 2); break;
+                case 1: RemoveWalls(cell, -1, 2, -1, -1); break;
+            }
+            InitialGateRooms.Add(cell);
+            cell.inRoom = true;
+        }
+    }
+
+
+
+
+    private void GenerateRooms(int roomMinLength, int roomMaxLength, int totalRooms)
+    {
+        for (int i = 0; i < totalRooms; i++)
+        {
+            CreateARoom(roomMinLength, roomMaxLength);
+        }
+    }
+    private void CreateARoom(int roomMinLength, int roomMaxLength)
+    {
+        int length = Random.Range(roomMinLength, roomMaxLength + 1);
+        int width = Random.Range(roomMinLength, roomMaxLength + 1);
+
+        Vector2 start = new Vector2(Random.Range(0, grid.length), Random.Range(0, grid.width));
+
+        bool flag = false;
+        int i = (int)start.x;
+        int j = (int)start.y;
+        for (i = (int)start.x; i < (int)start.x + length; i++)
+        {
+            for (j = (int)start.y; j < (int)start.y + width; j++)
+            {
+                if (mapCells[i, j].inRoom)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) break;
+        }
+
+        if (flag)
+        {
+            //CreateARoom(roomMinLength, roomMaxLength);
+        }
+
+
+        //========Create the room=======//
+
+    }
+
+
+
+
+    private void GeneratePath()
+    {
+        foreach (var cell in InitialGateRooms)
+        {
+            Increment(cell);
+        }
+    }
+    private void Increment(MapCell cell)
+    {
+        List<int> incrementDir = new List<int>();
+        for (int i = 0; i < cell.wall.Length; i++)
+        {
+            if (cell.wall[i] == WallType.wall) 
+                incrementDir.Add(i);
+        }
+
+
     }
 }
 
@@ -123,6 +297,9 @@ public class MapCell
     public Vector3 position;
     public int width;
     public Vector2 id;
+    public bool visited;
+    public bool inRoom;
+
 
     // 0 => right , 1 => top , 2 => left , 3 => bottom
 
@@ -131,8 +308,6 @@ public class MapCell
     public WallType[] wall = new WallType[4];
 
     public GameObject[] wallG = new GameObject[4];
-
-    public bool visited;
 
     public GameObject cellObject;
 
@@ -156,10 +331,10 @@ public class MapCell
 
         switch (wallIndex)
         {
-            case 0: return position + new Vector3(halfSize, 0, 0);
-            case 1: return position + new Vector3(0, 0, halfSize);
-            case 2: return position - new Vector3(halfSize, 0, 0);
-            case 3: return position - new Vector3(0, 0, halfSize);
+            case 0: return position + new Vector3(halfSize, 1, 0);
+            case 1: return position + new Vector3(0, 1, halfSize);
+            case 2: return position - new Vector3(halfSize, -1, 0);
+            case 3: return position - new Vector3(0, -1, halfSize);
             default: return position;
         }
     }
@@ -168,10 +343,10 @@ public class MapCell
     {
         switch (wallIndex)
         {
-            case 0: return Quaternion.Euler(0, 90, 0);
-            case 1: return Quaternion.identity;
-            case 2: return Quaternion.Euler(0, -90, 0);
-            case 3: return Quaternion.Euler(0, 180, 0);
+            case 0: return Quaternion.identity;
+            case 1: return Quaternion.Euler(0, 90, 0);
+            case 2: return Quaternion.Euler(0, 180, 0);
+            case 3: return Quaternion.Euler(0, -90, 0);
             default: return Quaternion.identity;
         }
     }
