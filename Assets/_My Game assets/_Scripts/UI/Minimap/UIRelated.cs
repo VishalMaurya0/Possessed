2b23f5f51da5ae94c00131d00a983fba5f1cf1f8
VishalMaurya0 <vishalmaurya22.24@kgpian.iitkgp.ac.in>
2025-07-03 21:30:23 +0530

    Made a timer for showing Full map

--- Assets/_My Game assets/_Scripts/UI/Minimap/UIRelated.cs
+++ Assets/_My Game assets/_Scripts/UI/Minimap/UIRelated.cs
@@ -1,43 +1,79 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIRelated : MonoBehaviour
 {
-    [Header("References")]
+    [Header("For Minimap UI")]
+    public RectTransform FullMapBackground;
+    public Image FullMapBackgroundImage;
+    public TMP_Text FullMapText;
+    public float height;
+    public float originalHeight;
+    public float decAlpha;
+    public float originalAlpha;
+    public Color normalColor;
+    [ColorUsage(true, true)]public Color redColor;
+
+    [Header("Animator References")]
     public Animator MinimapAnim;
 
     [Header("Values")]
     public bool isFullMiniMapShowing;
+    public float fractionTime;
+    public float fractionTimeLimit = 0.2f;
 
     [Header("Times")]
-    public float timeMiniMapShown;
+    public float timeLeftToShowMiniMap;
     public float totalTimeToShowMap = 10;
     public float rechargeRate = 0.02f;
 
 
     private void Start()
     {
-        timeMiniMapShown = totalTimeToShowMap;
+        timeLeftToShowMiniMap = totalTimeToShowMap;
+        originalHeight = FullMapBackground.sizeDelta.y;
+        originalAlpha = FullMapText.alpha;
+        normalColor = FullMapBackgroundImage.color;
     }
 
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Tab) && timeMiniMapShown > 2)
+        fractionTime = timeLeftToShowMiniMap / totalTimeToShowMap;
+        if (Input.GetKeyUp(KeyCode.Tab) && fractionTime > fractionTimeLimit)
         {
             LoadMinimapPanel(!isFullMiniMapShowing);
-        }else if (timeMiniMapShown < 2)
+        } else if (timeLeftToShowMiniMap <= 0 || Input.GetKeyUp(KeyCode.Tab))
         {
             LoadMinimapPanel(false);
         }
 
         if (isFullMiniMapShowing)
         {
-            timeMiniMapShown -= Time.deltaTime;
+            timeLeftToShowMiniMap -= Time.deltaTime;
         }
         
         if (!isFullMiniMapShowing)
         {
-            timeMiniMapShown += Time.deltaTime * rechargeRate;
+            timeLeftToShowMiniMap += Time.deltaTime * rechargeRate;
+            timeLeftToShowMiniMap = Mathf.Clamp(timeLeftToShowMiniMap, 0, totalTimeToShowMap);
         }
+
+        height = fractionTime * originalHeight;
+        FullMapBackground.sizeDelta = new(FullMapBackground.sizeDelta.x, height);
+
+        decAlpha = fractionTime * originalAlpha;
+        //FullMapText.alpha = decAlpha;
+
+        if (fractionTime < fractionTimeLimit || isFullMiniMapShowing)
+        {
+            FullMapBackgroundImage.GetComponent<Image>().color = redColor;
+        }else
+        {
+            FullMapBackgroundImage.GetComponent<Image>().color = normalColor;
+        }
+
+        FullMapText.text = $"Full Map : {Mathf.Round(fractionTime * 10 * 100f) / 100f}";
     }
 
     public void LoadMinimapPanel(bool Load)
